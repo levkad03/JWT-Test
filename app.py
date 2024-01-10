5a084d3a8db76cdeb52fbe9c84e08525b91f46f7
Lev Kadatscky <levkadatscky@gmail.com>
2024-01-10 13:17:44 +0200

    create models.py, renamed main.py to app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from extensions import db
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db.init_app(app)
