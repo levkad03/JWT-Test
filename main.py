ec9f3e268d559413019e832663941a7c1f61f927
Lev Kadatscky <levkadatscky@gmail.com>
2024-01-10 13:02:02 +0200

    added extensions.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from extensions import db
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db.init_app(app)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
