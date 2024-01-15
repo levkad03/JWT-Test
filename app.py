89adcd64fb52e1c1c6794b36614c39bf18e0a469
Lev Kadatscky <levkadatscky@gmail.com>
2024-01-15 14:06:46 +0200

    added error handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from extensions import db, jwt
 from auth import auth_bp
 from users import user_bp
@@ -10,11 +10,37 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['FLASK_JWT_SECRET_KEY'] = 'f8e8888797be153d6dff2abf'
 
+# initialize extensions
 db.init_app(app)
 jwt.init_app(app)
 
+# register blueprints
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(user_bp, url_prefix='/users')
+
+# jwt error handlers
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_data):
+    return jsonify({
+        'message': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+    
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'message': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+    
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'message': 'Request does not contain an access token.',
+        'error': 'authorization_header'
+    }), 401
 
 
 if __name__ == '__main__':
